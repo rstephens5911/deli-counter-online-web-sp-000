99f5ee911b2ec78dddacb1e9d215031f75e5ec80
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 20:44:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index do |name, index| 
+    katz_deli.each_with_index do |name, index|
       puts "The line is currently: #{index.to_i + 1}. #{name}"
-      
+
       # puts "The line is currently: #{index.to_i + 1}. #{name} "
     end
   end
