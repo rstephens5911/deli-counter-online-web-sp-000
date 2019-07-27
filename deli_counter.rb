ea6610144f251c78160c98867a132d3383067aa0
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 20:39:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,5 @@ def line(katz_deli)
   else
     puts "the line is currently: #{katz_deli.each_with_index do |name, index|}"
       # puts "The line is currently: #{index.to_i + 1}. #{name} "
-    end
   end
 end
