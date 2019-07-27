7a67781cc7f9b4c50f83aee1816e1dc259c41315
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 19:41:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   # katz_deli = []
   if katz_deli.size > 0
     # katz_deli.each_with_index do |name, index|
-      puts "The line is currently:  #{katz_deli.map.with_index{ |index, name| index+1. name }. "
+      puts "The line is currently:  #{katz_deli.map.with_index{ |index, name| index+1. name } }"
 
     else
       puts "The line is currently empty."
