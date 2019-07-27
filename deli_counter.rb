d3e5fd78d4934ca3ee6f971d454c6445ccc3cf68
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 21:17:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,5 +20,8 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
+  if katz_deli.size > 0
   puts "currently serving #{katz_deli.shift[0]}"
+else
+  puts "There is nobody waiting to be served!"
 end
