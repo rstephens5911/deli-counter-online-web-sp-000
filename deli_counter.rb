8cd451f768d03743d055455e74b98d2243b96265
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 21:00:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(katz_deli)
   else katz_deli.size > 0
     i = 0
     str = "The line is currently: "
-    while i < katz_deli.size
-      str << "#{i + 1}. #{katz_deli[i]} "
+    while i < katz_deli.size - 1
+      str << "#{i + 1}. #{katz_deli[i]}"
       i += 1
     end
-    puts str
+    str << "#{katz_deli.size + 1}. #{katz_deli[-1]}"
   end
 end
