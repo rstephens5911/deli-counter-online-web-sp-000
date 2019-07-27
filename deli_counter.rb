5424989f92f6c5caec4e34061930e0b1b100be56
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 20:05:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,3 @@
 def line(katz_deli)
-  # katz_deli = []
-  # if katz_deli.size > 0
-    # katz_deli.each_with_index do |name, index|
-    puts "the line is currently: #{katz_deli.each_with_index {|name, index|  {index + 1}.  {name}}}"
-
-
-    #else
-      puts "The line is currently empty."
-
-end
-
-def take_a_number(array, name)
-#  katz_deli << name
-# puts "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
+  katz_deli.each {|item| puts item}
 end
