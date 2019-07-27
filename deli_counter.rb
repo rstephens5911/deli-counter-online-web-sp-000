7bc9e99a4177af9592b609eda79031c709d813f2
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 16:39:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   # katz_deli = []
   if katz_deli.size > 0
     # katz_deli.each_with_index do |name, index|
-      puts "The line is currently:  #{katz_deli.each_with_index do |name, index|}. "
+      puts "The line is currently:  #{katz_deli.map_with_index(name, index)}. "
     end
 
     else
@@ -12,5 +12,5 @@ end
 
 def take_a_number(array, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.map_with_index(name, index)} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
 end
