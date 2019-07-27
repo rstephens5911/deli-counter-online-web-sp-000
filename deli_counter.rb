ee232c051d96d2796da90bb70d468a606d77f3fa
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 20:09:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index {|index, item| puts "#{index + 1}. #{item} "}
+    katz_deli.each_with_index {|index, item| puts "#{index.to_i + 1}. #{item} "}
   end
 end
