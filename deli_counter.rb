059925a174386e9414b382224f831cf369437d3c
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 16:32:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   # katz_deli = []
   if katz_deli.size > 0
     katz_deli.each_with_index do |name, index|
-      puts "The line is currently:  #{index + 1}. #{name}"
+      puts "The line is currently:  #{index + 1}. #{name} "
     end
 
     else
