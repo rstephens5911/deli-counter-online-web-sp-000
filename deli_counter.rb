26d3899a9f9ebe303093788001eca5560dd12bf8
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 21:15:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,7 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
 end
+
+def now_serving(katz_deli)
+  puts "currently serving #{katz_deli.shift[0]}"
+end
